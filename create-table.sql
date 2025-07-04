ebc337ed5d421ebf82fd34113fe20fa61235e0de
pawaromkar14 <omkarpawar1426@gmail.com>
2025-07-04 09:54:24 +0530

    Q2

---
+++ create-table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS demo;
+USE demo;
+
+CREATE TABLE students(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    marks INT
+);
+
+INSERT INTO students (name, marks) VALUES
+("Omkar", 90)
+("Aditya", 100)
+("Pranav", 110)
